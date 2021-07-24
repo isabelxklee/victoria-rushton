a8b78591407946c45bf09e38bfaf0c8358f88888
isabelxklee <hi@kleetime.com>
2021-07-24 14:22:21 -0400

    updated header spacing

--- src/components/FontHero/index.jsx
+++ src/components/FontHero/index.jsx
@@ -4,9 +4,9 @@ import {FontHeroContainer} from './styles'
 
 const FontHero = ({font}) => (
   <FontHeroContainer>
-    <p>{font.name}</p>
-    <h2>{font.preview}</h2>
     <InternalLink to={`/type/${font.name.toLowerCase().replace(/\s+/g, '-')}`}>
+      <p>{font.name}</p>
+      <h2>{font.preview}</h2>
       View this font
     </InternalLink>
   </FontHeroContainer>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const HeaderContainer = styled.div`
   color: #1e1e1e;
-  padding: 32px 80px;
-  position: absolute;
+  padding: 80px 80px 0px 80px;
+  position: relative;
   top: 0;
   width: calc(100vw - 160px);
 `
