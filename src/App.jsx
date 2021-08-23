794c8fa7c071338e2632d96eec834b8163322f2d
isabelxklee <hi@kleetime.com>
2021-08-23 16:00:12 -0400

    added routes for each font page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {Switch, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import FontProfile from './pages/FontProfile'
 
 const App = () => {
   // const [data, setData] = useState(null)
@@ -42,6 +43,11 @@ const App = () => {
         <Route exact path="/">
           <Home fonts={fonts} />
         </Route>
+        {fonts.map((font) => (
+          <Route key={font.id} path={`/type/${font.name.toLowerCase().replace(/\s+/g, '-')}`}>
+            <FontProfile font={font} key={font.id} />
+          </Route>
+        ))}
       </Switch>
       <Footer />
     </>
---
+++ src/pages/FontProfile.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const FontProfile = ({font}) => {
+  return <h1>{font.name}</h1>
+}
+
+export default FontProfile
