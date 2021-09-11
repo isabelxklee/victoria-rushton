450b45db717e049caca4a6ae0e8eb5b07f714928
isabelxklee <hi@kleetime.com>
2021-09-11 09:51:39 -0400

    created styled component for sanity's block content

--- src/pages/FontProfile/index.jsx
+++ src/pages/FontProfile/index.jsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
-import sanityClient from '../client.js'
-import BlockContent from '@sanity/block-content-to-react'
-import TypeTester from '../components/TypeTester'
+import sanityClient from '../../client.js'
+// import TypeTester from '../../components/TypeTester'
+import {StyledBlock} from './styles'
 
 const FontProfile = () => {
   const [data, setData] = useState(null)
@@ -19,32 +19,40 @@ const FontProfile = () => {
             "slants": slants[]->title,
             preview1,
             preview1Size,
-            "preview 1 weight": preview1Weight[]->title,
-            "preview 1 slant": preview1Slant[]->title,
+            "preview1weight": preview1Weight[]->title,
+            "preview1slant": preview1Slant[]->title,
             preview2,
             preview2Size,
-            "preview 2 weight": preview2Weight[]->title,
-            "preview 2 slant": preview2Slant[]->title,
+            "preview2weight": preview2Weight[]->title,
+            "preview2slant": preview2Slant[]->title,
             preview3,
             preview3Size,
-            "preview 3 weight": preview3Weight[]->title,
-            "preview 3 slant": preview3Slant[]->title,
+            "preview3weight": preview3Weight[]->title,
+            "preview3slant": preview3Slant[]->title,
         }`,
         {slug}
       )
-      .then((data) => setData(data))
+      .then((data) => setData(data[0]))
       .catch(console.error)
   }, [slug])
+
+  if (!data) return <div>Loading...</div>
+
+  console.log(data)
 
   return (
     <>
       {data && (
         <div>
           <h1>{data.title}</h1>
-          <BlockContent
+          <p>
+            {data.title} {data.preview1weight}
+          </p>
+          <StyledBlock
             blocks={data.preview1}
             projectId={sanityClient.clientConfig.projectId}
             dataset={sanityClient.clientConfig.dataset}
+            $fontSize={data.preview1Size}
           />
           {/* <TypeTester font={data} /> */}
         </div>
