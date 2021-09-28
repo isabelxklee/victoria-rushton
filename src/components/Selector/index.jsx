8860cb6dd21ba60c684e3caf957875d78e168ca6
isabelxklee <hi@kleetime.com>
2021-09-27 23:24:58 -0400

    trying to customize selector styles in type tester

--- src/components/Selector/index.jsx
+++ src/components/Selector/index.jsx
@@ -3,6 +3,18 @@ import {ReactSelect} from './styles'
 import {Label, Margin} from '../../styles'
 
 const Selector = ({title, options, defaultValue, handleChange, isDisabled}) => {
+  // const customStyles = {
+  //   control: (styles) => ({
+  //     ...styles,
+  //     backgroundColor: 'black',
+  //     color: 'white',
+  //   }),
+  //   option: (base, state) => ({
+  //     ...base,
+  //     backgroundColor: state.isSelected ? 'blue' : 'red',
+  //   }),
+  // }
+
   return (
     <Margin $margin="0 0 24px 0">
       <Label>
@@ -13,6 +25,7 @@ const Selector = ({title, options, defaultValue, handleChange, isDisabled}) => {
           options={options}
           placeholder={`Select a ${title}`}
           isDisabled={isDisabled}
+          // style={customStyles}
         />
       </Label>
     </Margin>
--- src/pages/FontProfile/styles.js
+++ src/pages/FontProfile/styles.js
@@ -1,10 +1,4 @@
 import styled from 'styled-components'
-import BlockContent from '@sanity/block-content-to-react'
-
-export const StyledBlock = styled(BlockContent)`
-  font-size: ${(props) => props.$fontSize}px;
-  font-weight: ${(props) => props.$fontWeight};
-`
 
 export const TypeTesterBackground = styled.div`
   background: #1f1f1f;
