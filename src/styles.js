bacb49b758c7aac924a563466cba12099b82c84d
isabelxklee <hi@kleetime.com>
2021-10-04 10:54:22 -0400

    updated footer style

--- src/styles.js
+++ src/styles.js
@@ -33,6 +33,10 @@ export const P = styled.p`
   font-size: 18px;
   font-weight: 300;
   line-height: 1.4;
+
+  @media (max-width: 800px) {
+    font-size: 16px;
+  }
 `
 
 export const PSpace = styled(P)`
