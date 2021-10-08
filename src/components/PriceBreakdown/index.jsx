630cc5a6ae1d689c757a068ace6a84f161e13bfe
isabelxklee <hi@kleetime.com>
2021-10-08 12:21:27 -0400

    created empty state for cart

--- src/components/PriceBreakdown/index.jsx
+++ src/components/PriceBreakdown/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {SelectedItem, TotalPrice} from './styles'
-import {P} from '../../styles'
+import {P, PSpace, Button, H3} from '../../styles'
 
 const PriceBreakdown = ({
   font,
@@ -12,30 +12,39 @@ const PriceBreakdown = ({
 }) => {
   return (
     <>
-      <h3>Cart</h3>
-      <>
-        {selectedFonts &&
-          selectedFonts.map((weightTitle) => (
-            <SelectedItem key={weightTitle}>
-              <P>
-                {font.title} {weightTitle}
-              </P>
-              <P>$100</P>
+      <H3>Cart</H3>
+      {selectedFonts || selectedLicense ? (
+        <>
+          <>
+            {selectedFonts &&
+              selectedFonts.map((weightTitle) => (
+                <SelectedItem key={weightTitle}>
+                  <P>
+                    {font.title} {weightTitle}
+                  </P>
+                  <P>$100</P>
+                </SelectedItem>
+              ))}
+          </>
+          {selectedLicense && (
+            <SelectedItem>
+              <P>{selectedLicense.title} License</P>
+              <P>${licensePrice}</P>
             </SelectedItem>
-          ))}
-      </>
-      {selectedLicense && (
-        <SelectedItem>
-          <P>{selectedLicense.title} License</P>
-          <P>${licensePrice}</P>
-        </SelectedItem>
+          )}
+          <SelectedItem>
+            <TotalPrice>Subtotal</TotalPrice>
+            <TotalPrice>
+              ${licensePrice ? licensePrice + fontPrice : fontPrice} {currency}
+            </TotalPrice>
+          </SelectedItem>
+        </>
+      ) : (
+        <>
+          <PSpace>Add something to your cart.</PSpace>
+        </>
       )}
-      <SelectedItem>
-        <TotalPrice>Subtotal</TotalPrice>
-        <TotalPrice>
-          ${licensePrice ? licensePrice + fontPrice : fontPrice} {currency}
-        </TotalPrice>
-      </SelectedItem>
+      <Button $disabled={selectedLicense || selectedFonts ? false : true}>Checkout</Button>
     </>
   )
 }
--- src/styles.js
+++ src/styles.js
@@ -62,7 +62,8 @@ export const Button = styled.button`
   font-weight: 300;
   font-size: 16px;
   padding: 14px 20px 10px 20px;
-  cursor: pointer;
+  cursor: ${({$disabled}) => ($disabled ? 'default' : 'pointer')};
+  opacity: ${({$disabled}) => ($disabled ? 0.3 : 1)};
 `
 
 export const SecondaryButton = styled(Button)`
