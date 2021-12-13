fddfae12e8ce4a8ec6239c0ca1de64e734ca4378
Isabel Lee <isabelxklee@gmail.com>
2021-12-12 21:10:19 -0500

    show correct font and price info on stripe checkout

--- client/src/components/Checkout/index.jsx
+++ client/src/components/Checkout/index.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 import {loadStripe} from '@stripe/stripe-js'
 import {Button} from '../../styles'
 
-const CheckoutForm = ({disableCheckout, selectedLicense, selectedFonts, totalPrice, font}) => {
-  const item = {name: 'test', price: 1000}
+const CheckoutForm = ({disableCheckout, selectedLicense, selectedFonts, font}) => {
+  // console.log(selectedFonts, selectedLicense, font)
 
   const handleSubmit = async (event) => {
     event.preventDefault()
+    const checkoutData = await {
+      name: font,
+      weights: selectedFonts,
+      license: selectedLicense,
+    }
     const stripe = await loadStripe(process.env.REACT_APP_STRIPE_SECRET)
 
     const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/create-checkout-session`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(item),
-      // body: JSON.stringify({
-      //   price: totalPrice,
-      //   name: font,
-      // }),
+      // body: JSON.stringify(item),
+      body: JSON.stringify(checkoutData),
     })
       .then((r) => r.json())
       .then((session) => stripe.redirectToCheckout({sessionId: session.id}))
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const stripe = require("stripe")(process.env.SECRET_STRIPE_KEY);
 const port = process.env.PORT;
+const domain = process.env.DOMAIN;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,18 +15,18 @@ app.use(bodyParser.json());
 
 app.post("/create-checkout-session", async (req, res) => {
   const data = req.body;
-  console.log(req.body);
 
   // const transformLineItems = () => {
-  //   data.fonts.map((font) => {
+  //   data.weights.map((weight) => {
   //     return {
   //       price_data: {
   //         currency: "usd",
   //         product_data: {
-  //           name: font,
+  //           name: `${data.name} ${weight}`,
   //         },
   //         unit_amount: data.license.price,
   //       },
+  //       quantity: 1,
   //     };
   //   });
   // };
@@ -39,13 +40,13 @@ app.post("/create-checkout-session", async (req, res) => {
           product_data: {
             name: data.name,
           },
-          unit_amount: data.price * 100,
+          unit_amount: data.weights.length * data.license.price * 100,
         },
         quantity: 1,
       },
     ],
     mode: "payment",
-    success_url: "https://example.com/success",
+    success_url: `${domain}`,
     cancel_url: "https://example.com/cancel",
   });
 
