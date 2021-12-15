f75659a2c963c7095b3dd0a13912f3698973cb42
Isabel Lee <isabelxklee@gmail.com>
2021-12-15 01:08:54 -0500

    updated font style for announcement

--- src/components/Announcement/index.jsx
+++ src/components/Announcement/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import {P} from '../../styles'
+import {P, H3} from '../../styles'
 import {AnnouncementContainer} from './styles'
 
 const Announcement = () => {
   return (
     <AnnouncementContainer>
+      <H3>Also available as a variable font!</H3>
       <P>
-        Also available as a variable font! Purchase 5 styles to receive it along with your order -
-        the value of all 10 styles for half the price!
+        Purchase 5 styles to receive it along with your order - the value of all 10 styles for half
+        the price.
       </P>
     </AnnouncementContainer>
   )
--- src/components/Announcement/styles.js
+++ src/components/Announcement/styles.js
@@ -4,6 +4,6 @@ import {Colors} from '../../styles'
 export const AnnouncementContainer = styled.div`
   background-color: ${Colors.black};
   color: ${Colors.white};
-  padding: 80px;
+  padding: 40px;
   margin: 80px 40px 0px 40px;
 `
