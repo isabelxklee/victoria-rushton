710773c1821f171dd27dfb91cf55ecbfd9ea140e
Isabel Lee <isabelxklee@gmail.com>
2021-12-21 21:25:48 -0500

    added variable font GIFs

--- src/components/Announcement/index.jsx
+++ src/components/Announcement/index.jsx
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import {P, H3} from '../../styles'
-import {AnnouncementContainer} from './styles'
+import {AnnouncementContainer, GIF} from './styles'
 import sanityClient from '../../client.js'
 import {announcementQuery} from '../../queries.js'
+import gif1 from '../../assets/Cecilie-Sans-Variable-1.gif'
+import gif2 from '../../assets/Cecilie-Sans-Variable-2.gif'
 
 const Announcement = () => {
   const [message, setMessage] = useState(null)
@@ -17,10 +19,14 @@ const Announcement = () => {
   return (
     <>
       {message && (
-        <AnnouncementContainer>
-          <H3>{message.title}</H3>
-          <P>{message.description}</P>
-        </AnnouncementContainer>
+        <>
+          <GIF src={gif1} alt="" />
+          <GIF src={gif2} alt="" />
+          <AnnouncementContainer>
+            <H3>{message.title}</H3>
+            <P>{message.description}</P>
+          </AnnouncementContainer>
+        </>
       )}
     </>
   )
--- src/components/Announcement/styles.js
+++ src/components/Announcement/styles.js
@@ -2,7 +2,13 @@ import styled from 'styled-components'
 import {Colors} from '../../styles'
 
 export const AnnouncementContainer = styled.div`
-  background-color: ${Colors.black};
+  /* background-color: ${Colors.black}; */
+  background-color: #1b1b1b;
   color: ${Colors.white};
   padding: 40px;
+`
+
+export const GIF = styled.img`
+  width: 100%;
+  margin-bottom: -4px;
 `
