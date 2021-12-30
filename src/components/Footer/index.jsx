e4d4210d0ac8ca6da0d834be3d045e0784a64d05
Isabel Lee <isabelxklee@gmail.com>
2021-12-30 16:55:04 -0500

    updated links in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,11 +6,21 @@ const Footer = () => (
   <FooterContainer>
     <P>
       © Victoria Rushton 2021. See me on{' '}
-      <TextLink href="/" rel="no_referrer" $light={true} target="_blank">
+      <TextLink
+        href="https://twitter.com/victoriaalissia"
+        rel="no_referrer"
+        $light={true}
+        target="_blank"
+      >
         Twitter
       </TextLink>{' '}
       and{' '}
-      <TextLink href="/" rel="no_referrer" $light={true} target="_blank">
+      <TextLink
+        href="https://www.instagram.com/victoriaalissia/?hl=en"
+        rel="no_referrer"
+        $light={true}
+        target="_blank"
+      >
         Instagram.
       </TextLink>
     </P>
