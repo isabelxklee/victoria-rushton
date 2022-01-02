1fbca8ea7e7b2dac17337b5f8f52b49ab2e55efe
Isabel Lee <isabelxklee@gmail.com>
2022-01-02 14:47:10 -0500

    updated cecilie sans font files

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,31 +1,22 @@
 import {createGlobalStyle} from 'styled-components'
 import {Colors} from './styles'
+
 // cecilie sans
 // 800
-import cecilie8001 from './fonts/cecilie-sans/800-cecilie-black.woff'
-import cecilie8002 from './fonts/cecilie-sans/800-cecilie-black.woff2'
-import cecilieItalic8001 from './fonts/cecilie-sans/800-cecilie-black-italic.woff'
-import cecilieItalic8002 from './fonts/cecilie-sans/800-cecilie-black-italic.woff2'
+import cecilie800 from './fonts/cecilie-sans/Cecilie-Black.woff'
+import cecilieItalic800 from './fonts/cecilie-sans/Cecilie-Black-Italic.woff'
 // 700
-import cecilie7001 from './fonts/cecilie-sans/700-cecilie-bold.woff'
-import cecilie7002 from './fonts/cecilie-sans/700-cecilie-bold.woff2'
-import cecilieItalic7001 from './fonts/cecilie-sans/700-cecilie-bold-italic.woff'
-import cecilieItalic7002 from './fonts/cecilie-sans/700-cecilie-bold-italic.woff2'
+import cecilie700 from './fonts/cecilie-sans/Cecilie-Bold.woff'
+import cecilieItalic700 from './fonts/cecilie-sans/Cecilie-Bold-Italic.woff'
 // 400
-import cecilie4001 from './fonts/cecilie-sans/400-cecilie-regular.woff'
-import cecilie4002 from './fonts/cecilie-sans/400-cecilie-regular.woff2'
-import cecilieItalic4001 from './fonts/cecilie-sans/400-cecilie-italic.woff'
-import cecilieItalic4002 from './fonts/cecilie-sans/400-cecilie-italic.woff2'
+import cecilie400 from './fonts/cecilie-sans/Cecilie-Regular.woff'
+import cecilieItalic400 from './fonts/cecilie-sans/Cecilie-Regular-Italic.woff'
 // 300
-import cecilie3001 from './fonts/cecilie-sans/300-cecilie-book.woff'
-import cecilie3002 from './fonts/cecilie-sans/300-cecilie-book.woff2'
-import cecilieItalic3001 from './fonts/cecilie-sans/300-cecilie-book-italic.woff'
-import cecilieItalic3002 from './fonts/cecilie-sans/300-cecilie-book-italic.woff2'
+import cecilie300 from './fonts/cecilie-sans/Cecilie-Book.woff'
+import cecilieItalic300 from './fonts/cecilie-sans/Cecilie-Book-Italic.woff'
 // 200
-import cecilie2001 from './fonts/cecilie-sans/200-cecilie-light.woff'
-import cecilie2002 from './fonts/cecilie-sans/200-cecilie-light.woff2'
-import cecilieItalic2001 from './fonts/cecilie-sans/200-cecilie-light-italic.woff'
-import cecilieItalic2002 from './fonts/cecilie-sans/200-cecilie-light-italic.woff2'
+import cecilie200 from './fonts/cecilie-sans/Cecilie-Light.woff'
+import cecilieItalic200 from './fonts/cecilie-sans/Cecilie-Light-Italic.woff'
 
 // embury text
 // 700
@@ -73,81 +64,70 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Cecilie Sans';
     font-style: normal;
     font-weight: 800;
-    src: url(${cecilie8001}) format('woff'),
-        url(${cecilie8002}) format('woff2');
+    src: url(${cecilie800}) format('woff')
   }
 
   @font-face {
     font-family: 'Cecilie Sans';
     font-style: italic;
     font-weight: 800;
-    src: url(${cecilieItalic8001}) format('woff'),
-        url(${cecilieItalic8002}) format('woff2');
+    src: url(${cecilieItalic800}) format('woff')
   }
 
   @font-face {
     font-family: 'Cecilie Sans';
     font-style: normal;
     font-weight: 700;
-    src: url(${cecilie7001}) format('woff'),
-        url(${cecilie7002}) format('woff2');
+    src: url(${cecilie700}) format('woff')
   }
 
   @font-face {
     font-family: 'Cecilie Sans';
     font-style: italic;
     font-weight: 700;
-    src: url(${cecilieItalic7001}) format('woff'),
-        url(${cecilieItalic7002}) format('woff2');
+    src: url(${cecilieItalic700}) format('woff')
   }
-
 
   @font-face {
       font-family: 'Cecilie Sans';
       font-style: normal;
       font-weight: 400;
-      src: url(${cecilie4001}) format('woff'),
-        url(${cecilie4002}) format('woff2');
+      src: url(${cecilie400}) format('woff')
   }
 
   @font-face {
       font-family: 'Cecilie Sans';
       font-style: italic;
       font-weight: 400;
-      src: url(${cecilieItalic4001}) format('woff'),
-        url(${cecilieItalic4002}) format('woff2');
+      src: url(${cecilieItalic400}) format('woff')
   }
 
   @font-face {
     font-family: 'Cecilie Sans';
     font-style: normal;
     font-weight: 300;
-    src: url(${cecilie3001}) format('woff'),
-        url(${cecilie3002}) format('woff2');
+    src: url(${cecilie300}) format('woff')
   }
 
   @font-face {
     font-family: 'Cecilie Sans';
     font-style: italic;
     font-weight: 300;
-    src: url(${cecilieItalic3001}) format('woff'),
-        url(${cecilieItalic3002}) format('woff2');
+    src: url(${cecilieItalic300}) format('woff')
   }
 
   @font-face {
     font-family: 'Cecilie Sans';
     font-style: normal;
     font-weight: 200;
-    src: url(${cecilie2001}) format('woff'),
-        url(${cecilie2002}) format('woff2');
+    src: url(${cecilie200}) format('woff')
   }
 
   @font-face {
     font-family: 'Cecilie Sans';
     font-style: italic;
     font-weight: 200;
-    src: url(${cecilieItalic2001}) format('woff'),
-        url(${cecilieItalic2002}) format('woff2');
+    src: url(${cecilieItalic200}) format('woff')
   }
 
   /* embury text */
