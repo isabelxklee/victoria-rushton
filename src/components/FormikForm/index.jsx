73e3d3d5c5262c1b1dd5aae9e7253ea0be9a970e
Isabel Lee <isabelxklee@gmail.com>
2022-01-18 00:42:03 -0500

    implemented useHistory hook from react router

--- src/components/FormikForm/index.jsx
+++ src/components/FormikForm/index.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import {useHistory} from 'react-router-dom'
 import {Button} from '../../styles'
 import {InputField, FieldContainer, Label, Error} from './styles'
 import {Formik, Form} from 'formik'
@@ -6,6 +7,7 @@ import * as Yup from 'yup'
 
 const FormikForm = ({selectedLicense, selectedFonts, font}) => {
   const [buttonLabel, setButtonLabel] = useState('Checkout')
+  let history = useHistory()
 
   const formSchema = Yup.object().shape({
     name: Yup.string().required('This is a required field.'),
@@ -21,21 +23,25 @@ const FormikForm = ({selectedLicense, selectedFonts, font}) => {
   const handleSubmit = async (values) => {
     setButtonLabel('Loading...')
 
-    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/download-trial-fonts`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        font: font.title,
-        selectedFonts: selectedFonts,
-        license: selectedLicense,
-        customerName: values.name,
-        customerEmail: values.email,
-      }),
-    })
+    // const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/download-trial-fonts`, {
+    //   method: 'POST',
+    //   headers: {'Content-Type': 'application/json'},
+    //   body: JSON.stringify({
+    //     font: font.title,
+    //     selectedFonts: selectedFonts,
+    //     license: selectedLicense,
+    //     customerName: values.name,
+    //     customerEmail: values.email,
+    //   }),
+    // })
 
-    if (!response) {
-      console.log(response.error)
-    }
+    // if (!response) {
+    //   console.log(response.error)
+    // }
+
+    setTimeout(() => {
+      history.push('/success')
+    }, 1200)
   }
 
   return (
@@ -45,10 +51,6 @@ const FormikForm = ({selectedLicense, selectedFonts, font}) => {
         validationSchema={formSchema}
         onSubmit={(values) => {
           handleSubmit(values)
-
-          setTimeout(() => {
-            window.location.reload()
-          }, 1200)
         }}
       >
         {({errors, touched, isValid, dirty}) => (
