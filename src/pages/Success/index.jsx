14f78dbdb3b7983ed89bd863a03970cdf8df4145
Isabel Lee <isabelxklee@gmail.com>
2022-01-18 01:01:57 -0500

    added history push to back button on success page

--- src/pages/Success/index.jsx
+++ src/pages/Success/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import {H2, PSpace, Button, Margin} from '../../styles'
 import {SuccessContainer} from './styles'
+import {useHistory} from 'react-router-dom'
 
 const Success = () => {
+  let history = useHistory()
+
   return (
     <SuccessContainer>
       <H2>Thank you for trying out my fonts for your next project!</H2>
@@ -12,7 +15,7 @@ const Success = () => {
         to get in touch with any questions.”
       </PSpace>
       <Margin $margin="40px 0" />
-      <Button>Back to fonts</Button>
+      <Button onClick={() => history.push('/')}>Back to fonts</Button>
     </SuccessContainer>
   )
 }
