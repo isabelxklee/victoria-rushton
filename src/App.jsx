5ec72756926d59d595fff98b169fab607f2b3e08
Isabel Lee <isabelxklee@gmail.com>
2022-01-18 00:20:43 -0500

    created component for success page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import FontProfile from './pages/FontProfile'
+import Success from './pages/Success'
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
           </Route>
           <Route component={Home} exact path="/" />
           <Route component={FontProfile} path="/:slug" />
+          <Route component={Success} path="/success" />
         </Switch>
       </Wrapper>
       <Footer />
---
+++ src/pages/Success/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <>
+      <h1>hello world</h1>
+      <p>here is the type page</p>
+    </>
+  )
+}
+
+export default Success
