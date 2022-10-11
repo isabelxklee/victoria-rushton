e6669546555430c74fbffb43497206104ebc81e1
isabelxklee <hi@kleetime.com>
2022-10-10 22:11:17 -0400

    refactored license component

--- src/components/License.jsx
+++ src/components/License.jsx
@@ -1,12 +1,11 @@
 import React, {useState, useEffect} from 'react'
-import sanityClient from '../../client.js'
-import {ParentContainer, SelectionContainer} from './styles'
-import {H2, H3, Margin} from '../../styles'
-import {PriceContainer} from '../PriceBreakdown/styles'
-import PriceBreakdown from '../PriceBreakdown'
-import SelectionForm from '../SelectionForm'
-import CheckoutLinks from '../CheckoutLinks'
-import {licensesQuery} from '../../queries'
+import sanityClient from '../client.js'
+import * as Component from './componentStyle'
+import * as Global from '../styles'
+import PriceBreakdown from './PriceBreakdown'
+import SelectionForm from './SelectionForm'
+import CheckoutLinks from './CheckoutLinks'
+import {licensesQuery} from '../queries'
 
 const License = ({font, weightOptions, slantOptions}) => {
   const [licenses, setLicenses] = useState(null)
@@ -27,9 +26,9 @@ const License = ({font, weightOptions, slantOptions}) => {
   }, [selectedFonts, selectedLicense])
 
   return (
-    <ParentContainer>
-      <SelectionContainer>
-        <H2>License this font</H2>
+    <Component.ParentContainer>
+      <Component.SelectionContainer>
+        <Global.H2>License this font</Global.H2>
         <SelectionForm
           selectedFonts={selectedFonts}
           setSelectedFonts={setSelectedFonts}
@@ -40,10 +39,10 @@ const License = ({font, weightOptions, slantOptions}) => {
           weightOptions={weightOptions()}
           slantOptions={slantOptions()}
         />
-      </SelectionContainer>
-      <PriceContainer>
-        <H3>Cart</H3>
-        <Margin $margin="16px 0">
+      </Component.SelectionContainer>
+      <Component.PriceContainer>
+        <Global.H3>Cart</Global.H3>
+        <Global.Margin $margin="16px 0">
           <PriceBreakdown
             selectedLicense={selectedLicense}
             setSelectedFonts={setSelectedFonts}
@@ -53,10 +52,10 @@ const License = ({font, weightOptions, slantOptions}) => {
             variableFont={variableFont}
             setVariableFont={setVariableFont}
           />
-        </Margin>
+        </Global.Margin>
         <CheckoutLinks />
-      </PriceContainer>
-    </ParentContainer>
+      </Component.PriceContainer>
+    </Component.ParentContainer>
   )
 }
 
--- src/components/componentStyle.js
+++ src/components/componentStyle.js
@@ -164,3 +164,33 @@ export const ButtonGroup = styled.div`
 export const Options = styled(ButtonGroup)`
   flex-direction: column;
 `
+
+export const ParentContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 80px 0 140px 0;
+
+  @media (max-width: 900px) {
+    display: block;
+  }
+`
+
+export const SelectionContainer = styled.div`
+  border-right: 2px solid ${Global.Colors.black};
+  padding: 0 40px;
+
+  @media (max-width: 900px) {
+    padding: 20px 20px 40px 20px;
+    border-right: unset;
+    border-bottom: 2px solid ${Global.Colors.black};
+  }
+`
+
+export const PriceContainer = styled.div`
+  margin: 40px 0;
+  padding: 0 40px;
+
+  @media (max-width: 900px) {
+    padding: 0 20px;
+  }
+`
