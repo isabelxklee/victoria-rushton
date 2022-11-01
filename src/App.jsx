c9036f1306b59ae84825591c39d1a4004abfb4de
isabelxklee <hi@kleetime.com>
2022-11-01 13:17:11 -0400

    implemented fonts reduer

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import {Wrapper, AppContainer} from './styles'
 import {GlobalStyle} from './globalStyle'
 import {Routes, Route} from 'react-router-dom'
 import sanityClient from './client.js'
 import {fontsQuery} from './queries.js'
+import {useSelector, useDispatch} from 'react-redux'
+import {addFonts} from './features/fonts/fontSlice.js'
 import About from './pages/About'
 import Home from './pages/Home'
 import FontProfile from './pages/FontProfile'
 import Success from './pages/Success'
 import Footer from './components/Footer'
 import Header from './components/Header'
-// import Counter from './features/counter/Counter'
 
 const App = () => {
-  const [fonts, setFonts] = useState(null)
+  const fonts = useSelector((state) => state.fonts.value)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     sanityClient
       .fetch(fontsQuery)
-      .then((data) => setFonts(data))
+      .then((data) => dispatch(addFonts(data)))
       .catch(console.error)
-  }, [])
+  }, [dispatch])
 
   console.log(fonts)
 
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
+import fontReducer from '../features/fonts/fontSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    fonts: fontReducer,
   },
 })
