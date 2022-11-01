995eb1b892c962a9136562326fef7dc8f6a69472
isabelxklee <hi@kleetime.com>
2022-11-01 13:00:40 -0400

    imported redux store and provider to index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
-import App from './App'
 import {BrowserRouter} from 'react-router-dom'
+import {store} from './app/store'
+import {Provider} from 'react-redux'
+import App from './App'
 import './index.css'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
