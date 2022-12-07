31a240794407e9d3712e7a1a618958bd5f68a32a
isabelxklee <hi@kleetime.com>
2022-12-07 15:46:36 -0500

    updated font files for gautreaux

--- src/styles/font-styles.js
+++ src/styles/font-styles.js
@@ -35,30 +35,6 @@ import emburyText3002 from '../fonts/embury-text/300-embury-text-book.woff2'
 import emburyTextItalic3001 from '../fonts/embury-text/300-embury-text-book-italic.woff'
 import emburyTextItalic3002 from '../fonts/embury-text/300-embury-text-book-italic.woff2'
 
-//gautreaux
-// 700
-import gautreaux7001 from '../fonts/gautreaux/700-gautreaux-bold.woff'
-import gautreaux7002 from '../fonts/gautreaux/700-gautreaux-bold.woff2'
-// 500
-import gautreaux5001 from '../fonts/gautreaux/500-gautreaux-medium.woff'
-import gautreaux5002 from '../fonts/gautreaux/500-gautreaux-medium.woff2'
-// 200
-import gautreaux2001 from '../fonts/gautreaux/200-gautreaux-light.woff'
-import gautreaux2002 from '../fonts/gautreaux/200-gautreaux-light.woff2'
-
-// marcia
-// 700
-import marcia7001 from '../fonts/marcia/700-marcia-bold.woff'
-// import marcia7002 from '../fonts/marcia/700-marcia-bold.woff2'
-import marciaItalic7001 from '../fonts/marcia/700-marcia-bold-italic.woff'
-// import marciaItalic7002 from '../fonts/marcia/700-marcia-bold-italic.woff2'
-
-// 400
-import marcia4001 from '../fonts/marcia/400-marcia-regular.woff'
-// import marcia4002 from '../fonts/marcia/400-marcia-regular.woff2'
-import marciaItalic4001 from '../fonts/marcia/400-marcia-regular-italic.woff'
-// import marciaItalic4002 from '../fonts/marcia/400-marcia-regular-italic.woff2'
-
 export const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'Cecilie Sans';
@@ -186,24 +162,24 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Gautreaux';
     font-style: normal;
     font-weight: 200;
-    src: url(${gautreaux2001}) format('woff'),
-      url(${gautreaux2002}) format('woff2');
+    src: url('https://storage.googleapis.com/victoria-rushton-bucket/gautreaux/200-gautreaux-light.woff') format('woff'),
+      url('https://storage.googleapis.com/victoria-rushton-bucket/gautreaux/200-gautreaux-light.woff2') format('woff2');
   }
 
   @font-face {
     font-family: 'Gautreaux';
     font-style: normal;
     font-weight: 500;
-    src: url(${gautreaux5001}) format('woff'),
-      url(${gautreaux5002}) format('woff2');
+    src: url('https://storage.googleapis.com/victoria-rushton-bucket/gautreaux/500-gautreaux-medium.woff') format('woff'),
+      url('https://storage.googleapis.com/victoria-rushton-bucket/gautreaux/500-gautreaux-medium.woff2') format('woff2');
   }
 
   @font-face {
     font-family: 'Gautreaux';
     font-style: normal;
     font-weight: 700;
-    src: url(${gautreaux7001}) format('woff'),
-      url(${gautreaux7002}) format('woff2');
+    src: url('https://storage.googleapis.com/victoria-rushton-bucket/gautreaux/700-gautreaux-bold.woff') format('woff'),
+      url('https://storage.googleapis.com/victoria-rushton-bucket/gautreaux/700-gautreaux-bold.woff2') format('woff2');
   }
 
   /* marcia */
