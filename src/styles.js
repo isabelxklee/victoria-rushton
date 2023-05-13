9fa15ce13b4b4d88d09f37c4fd7066891bb16f01
isabelxklee <hi@kleetime.com>
2023-05-13 18:10:04 -0400

    added global styles

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,20 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export const Wrapper = styled.div`
   padding: 80px;
+`;
+
+export const GlobalStyles = createGlobalStyle`
+@font-face {
+      font-family: 'Cecilie Sans';
+      font-style: normal;
+      font-weight: 400;
+      src: url('https://storage.googleapis.com/victoria-rushton-bucket/cecilie-sans/Cecilie-Regular.woff') format('woff')
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Cecilie Sans', sans-serif;
+  }
 `;
