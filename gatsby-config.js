c5e50ba1422e86fc4398582f327d80114d630744
isabelxklee <hi@kleetime.com>
2023-05-13 14:58:33 -0400

    updated index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,19 @@
 module.exports = {
   siteMetadata: {
     title: `victoria-rushton`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "hBGJYw7pEwG4qZn7cYJon0dHshXjrq3tmHxZd-fFNUc",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components"]
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "hBGJYw7pEwG4qZn7cYJon0dHshXjrq3tmHxZd-fFNUc",
+        spaceId: "6l1e28rigfdw",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
+  ],
 };
---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Home = () => {
+  return <h1>hello world</h1>;
+};
+
+export default Home;
