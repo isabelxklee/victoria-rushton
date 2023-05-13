9a43f74cdba9f800a036f5d65394f0837ff308dc
isabelxklee <hi@kleetime.com>
2023-05-13 18:45:39 -0400

    added global font weights

--- src/styles.js
+++ src/styles.js
@@ -5,22 +5,67 @@ export const COLORS = {
   BLACK: '#1E1E1E',
 }
 
+export const FONT_WEIGHTS = {
+  LIGHT: 200,
+  BOOK: 300,
+  REGULAR: 400,
+  MEDIUM: 500,
+  SEMIBOLD: 600,
+  BOLD: 700,
+  BLACK: 800,
+}
+
 export const Wrapper = styled.div`
   padding: 80px;
+`
+
+export const H1 = styled.h1`
+  font-size: 100px;
+  font-weight: ${FONT_WEIGHTS.REGULAR};
+  line-height: 0.9;
+  margin: ${({$margin}) => $margin};
+  font-family: ${({$font}) => $font};
+
+  @media (max-width: 900px) {
+    font-size: 60px;
+  }
+`
+
+export const H2 = styled.h2`
+  font-size: 32px;
+  font-weight: ${FONT_WEIGHTS.BOLD};
+  line-height: 1.1;
+`
+
+export const H3 = styled.h3`
+  font-size: 18px;
+  margin: 0 0 16px 0;
+`
+
+export const P = styled.p`
+  margin: 0;
+  font-size: 18px;
+  font-weight: 300;
+  line-height: 1.4;
+  text-align: left;
+
+  @media (max-width: 900px) {
+    font-size: 16px;
+  }
 `
 
 export const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: 'Cecilie Sans';
     font-style: normal;
-    font-weight: 300;
+    font-weight: ${FONT_WEIGHTS.BOOK};
     src: url('https://victoria-rushton-bucket.storage.googleapis.com/cecilie-sans/Cecilie-Book.woff') format('woff')
   }
 
   @font-face {
     font-family: 'Cecilie Sans';
     font-style: normal;
-    font-weight: 400;
+    font-weight: ${FONT_WEIGHTS.REGULAR};
     src: url('https://victoria-rushton-bucket.storage.googleapis.com/cecilie-sans/Cecilie-Regular.woff') format('woff')
   }
 
@@ -28,7 +73,7 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: 'Cecilie Sans', sans-serif;
-    font-weight: 300;
+    font-weight: ${FONT_WEIGHTS.BOOK};
     background: ${COLORS.WHITE};
     color: ${COLORS.BLACK};
   }
