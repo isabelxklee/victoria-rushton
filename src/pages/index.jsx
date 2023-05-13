ba0bdf37fedf947ca0215c9c532d020a5177ad8b
isabelxklee <hi@kleetime.com>
2023-05-13 15:02:43 -0400

    updated graphql query

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,34 @@
-import * as React from "react";
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 
-const Home = () => {
+const IndexPage = () => {
+  const data = useStaticQuery(pageQuery);
+  const fonts = data.allContentfulFont.nodes;
+
+  console.log(fonts);
+
   return <h1>hello world</h1>;
 };
 
-export default Home;
+export default IndexPage;
+
+const pageQuery = graphql`
+  query HomePageQuery {
+    allContentfulFont {
+      nodes {
+        name
+        slug
+        description {
+          description
+        }
+        heroCopy {
+          heroCopy
+        }
+        weights {
+          name
+          value
+        }
+      }
+    }
+  }
+`;
