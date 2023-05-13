1173f40fd19b358fb6d01023b49cb6ab85f51b99
isabelxklee <hi@kleetime.com>
2023-05-13 15:11:04 -0400

    added html elements to about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+
+const About = () => {
+  const data = useStaticQuery(pageQuery);
+  const about = data.allContentfulAbout.nodes[0];
+
+  return (
+    <>
+      <h2>{about.greeting.greeting}</h2>
+      <p>{about.bio.bio}</p>
+      <button>
+        <a href={about.buttonURL}>{about.buttonLabel}</a>
+      </button>
+    </>
+  );
+};
+
+export default About;
+
+const pageQuery = graphql`
+  query MyQuery {
+    allContentfulAbout {
+      nodes {
+        buttonURL
+        buttonLabel
+        bio {
+          bio
+        }
+        greeting {
+          greeting
+        }
+      }
+    }
+  }
+`;
