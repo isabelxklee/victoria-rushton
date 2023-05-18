93ab27e3aea4debf79fa6d75335ae28877967d40
isabelxklee <hi@kleetime.com>
2023-05-17 23:07:44 -0400

    installed eslint prettier

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,6 +10,42 @@ const About = () => {
 
   return (
     <PageTemplate>
+      <IntroContainer>
+        <ProfilePic
+          src="https://storage.googleapis.com/victoria-rushton-bucket/victoria_profile_pic_ds7twb.png"
+          rel="no_link referrer"
+          alt="A black and white portrait of a woman wearing glasses."
+        />
+        <div>
+          <H2>{about.greeting}</H2>
+          <BlockContent blocks={about.bio} />
+          <Button style={{margin: '26px 0'}}>
+            <TextLink href={about.buttonLink} inputWeight="300" $light={true} target="_blank">
+              {about.buttonText}
+            </TextLink>
+          </Button>
+        </div>
+      </IntroContainer>
+      <PressContainer>
+        <Container>
+          <H2>Speaking and Writing</H2>
+          {press &&
+            press.map((object) => (
+              <PressArticleContainer key={object._id}>
+                <PressArticleTitle
+                  href={object.link}
+                  target="_blank"
+                  rel="no_referrer"
+                  $linkStyle={object.link && 'on'}
+                >
+                  {object.title}
+                </PressArticleTitle>
+                <P>{object.description}</P>
+              </PressArticleContainer>
+            ))}
+        </Container>
+      </PressContainer>
+      {/*         
       <div>
         <H2>{about.greeting.greeting}</H2>
         <Text>{about.bio.bio}</Text>
@@ -27,7 +63,7 @@ const About = () => {
             <p>{press.description}</p>
           </div>
         ))}
-      </div>
+      </div> */}
     </PageTemplate>
   )
 }
