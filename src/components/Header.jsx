063aafc711e57761f26ae75db811334c647996a8
isabelxklee <hi@kleetime.com>
2023-05-17 23:30:10 -0400

    updating header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,62 @@
 import React from 'react'
+import styled from 'styled-components'
+// import {NavLink} from 'react-router-dom'
+// import {ReactComponent as LogoSVG} from '../../assets/icons/logo-full.svg'
+import {COLORS} from '../styles'
+
+export const HeaderContainer = styled.div`
+  color: ${COLORS.BLACK};
+  padding-bottom: 40px;
+  width: calc(100vw - 160px);
+  border-bottom: 2px solid ${COLORS.BLACK};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 900px) {
+    width: 100%;
+    padding: 40px 0;
+  }
+`
+
+// export const HeaderLink = styled(NavLink)`
+//   font-weight: 400;
+//   color: ${COLORS.BLACK};
+//   text-decoration: none;
+//   margin-left: 40px;
+//   font-size: 14px;
+//   text-transform: uppercase;
+
+//   @media (max-width: 900px) {
+//     margin-left: 20px;
+//   }
+// `
+
+// export const LogoLink = styled(NavLink)`
+//   display: block;
+// `
+
+// export const Logo = styled(LogoSVG)`
+//   width: 100%;
+
+//   @media (max-width: 900px) {
+//     width: 80%;
+//   }
+// `
 
 const Header = () => {
-  let activeClassName = 'active'
+  console.log(window.location.href)
 
   return (
     <HeaderContainer>
-      <LogoLink to="/">
+      {/* <LogoLink to="/">
         <Logo />
-      </LogoLink>
+      </LogoLink> */}
 
       <div>
-        {/* <HeaderLink to="/" className={({isActive}) => isActive && activeClassName}>
-        Type
-      </HeaderLink> */}
-        <HeaderLink to="/about" className={({isActive}) => isActive && activeClassName}>
+        {/* <HeaderLink to="/about" className={({isActive}) => isActive && activeClassName}>
           About
-        </HeaderLink>
+        </HeaderLink> */}
       </div>
     </HeaderContainer>
   )
--- src/components/PageTemplate.jsx
+++ src/components/PageTemplate.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Wrapper, GlobalStyles} from '../styles'
+import Header from './Header'
 
 const PageTemplate = ({children}) => {
   return (
     <Wrapper>
       <GlobalStyles />
+      <Header />
       {children}
     </Wrapper>
   )
