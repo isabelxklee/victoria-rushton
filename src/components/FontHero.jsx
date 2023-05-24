bff68a47aa18d7d9cd123fb2fb51417090c96835
isabelxklee <hi@kleetime.com>
2023-05-24 17:11:55 -0400

    cleaning up font fero

--- src/components/FontHero.jsx
+++ src/components/FontHero.jsx
@@ -1,34 +1,21 @@
-import React from 'react'
+import React, {useMemo} from 'react'
+import {Text} from '../styles'
 
 const FontHero = ({font}) => {
-  const calculateStyles = () => {
-    return font.weights.length * font.slants.length
-  }
+  const calculateStyles = useMemo(() => {
+    return font.weights.length
+    // * font.slants.length
+  })
+
+  console.log(font)
 
   return (
-    <Component.FontHeroContainer key={font._id}>
-      <Global.Padding $padding="20px 0px 0px 0px">
-        <Global.InternalLink to={'/' + font.slug} key={font._id}>
-          <Component.FlexContainer>
-            <Global.P>{font.title}</Global.P>
-            <Global.P $right={true}>{calculateStyles()} available styles</Global.P>
-          </Component.FlexContainer>
-
-          <Global.Margin $margin="60px 0">
-            <Component.PreviewText
-              $font={font.title}
-              $size={font.homeTextSize}
-              $weight={font.homeTextWeight}
-              $slant={font.homeTextSlant}
-              $lineHeight={font.homeTextLineHeight}
-            >
-              {font.homeText}
-            </Component.PreviewText>
-          </Global.Margin>
-          <Global.P style={{margin: '-10px 0'}}>View this font</Global.P>
-        </Global.InternalLink>
-      </Global.Padding>
-    </Component.FontHeroContainer>
+    <div>
+      <Text>{font.name}</Text>
+      <Text>{calculateStyles} available styles</Text>
+      <h2>{font.heroCopy.heroCopy}</h2>
+      <Text>View this font</Text>
+    </div>
   )
 }
 
