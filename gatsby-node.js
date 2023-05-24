a5ae1f22cc0f2b6f44a2d3c99427809e39c3f35a
isabelxklee <hi@kleetime.com>
2023-05-24 17:23:26 -0400

    updated gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,10 +8,10 @@ exports.createPages = async function ({actions, graphql}) {
       }
     }
   `)
-  data.allMarkdownRemark.nodes.forEach((node) => {
+  data.allContentfulFont.nodes.forEach((node) => {
     const slug = node.slug
     actions.createPage({
-      path: slug,
+      path: `/${slug}`,
       component: require.resolve(`./src/templates/fontPageTemplate.jsx`),
       context: {slug: slug},
     })
