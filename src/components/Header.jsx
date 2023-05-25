0319945ba944c53664be13eb41fe523801920b30
isabelxklee <hi@kleetime.com>
2023-05-25 00:05:48 -0400

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { BREAKPOINTS, COLORS } from '../styles';
+import { BREAKPOINTS, COLORS, RowFlex } from '../styles';
 
 import { InternalLink } from './Links';
+import { FlexContainer } from './styles';
 
-export const HeaderContainer = styled.div`
+const HeaderWrapper = styled(FlexContainer)`
   color: ${COLORS.BLACK};
   padding-bottom: 40px;
   width: calc(100vw - 160px);
   border-bottom: 2px solid ${COLORS.BLACK};
-  display: flex;
   justify-content: space-between;
   align-items: center;
 
-  @media (max-width: 900px) {
+  @media (max-width: ${BREAKPOINTS.MEDIUM}) {
     width: 100%;
     padding: 40px 0;
   }
 `;
 
-export const Logo = styled.img`
+const Right = styled(RowFlex)`
+  gap: 30px;
+`;
+
+const Logo = styled.img`
   width: 300px;
 
   @media (max-width: ${BREAKPOINTS.MEDIUM}) {
@@ -30,22 +34,22 @@ export const Logo = styled.img`
 
 const Header = () => {
   return (
-    <HeaderContainer>
+    <HeaderWrapper>
       <InternalLink url="/">
         <Logo
           alt=""
           src="https://images.ctfassets.net/6l1e28rigfdw/4M73zAbJDT93lMBUrSnGC6/a063c57966eee5adcffb38e9ed564cee/logo-full.svg"
         />
       </InternalLink>
-      <div>
+      <Right>
         <InternalLink textCase="uppercase" url="/">
           Home
         </InternalLink>
         <InternalLink textCase="uppercase" url="/about">
           About
         </InternalLink>
-      </div>
-    </HeaderContainer>
+      </Right>
+    </HeaderWrapper>
   );
 };
 
