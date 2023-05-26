547bc25d4f461cbcd965ece8d6bcec322e5d2edf
isabelxklee <hi@kleetime.com>
2023-05-26 17:07:41 -0400

    updating hero copy

--- src/components/FontHero.tsx
+++ src/components/FontHero.tsx
@@ -11,6 +11,18 @@ const StyledRowFlex = styled(RowFlex)`
   justify-content: space-between;
 `;
 
+const HeroCopy = styled.h2<{
+  $fontFamily: string;
+  $lineHeight: number;
+  $size: number;
+  $weight: number;
+}>`
+  font-family: ${({ $fontFamily }) => $fontFamily};
+  font-size: ${({ $size }) => `${$size}px`};
+  font-weight: ${({ $weight }) => $weight};
+  line-height: ${({ $lineHeight }) => $lineHeight};
+`;
+
 interface FontHeroProps {
   font: Font;
 }
@@ -29,7 +41,13 @@ const FontHero = ({ font }: FontHeroProps) => {
           <Text>{font.name}</Text>
           <Text>{calculateStyles} available styles</Text>
         </StyledRowFlex>
-        <h2>{font.heroCopy.heroCopy}</h2>
+        <HeroCopy
+          $fontFamily={font.name}
+          $lineHeight={font.heroCopyLineHeight}
+          $size={font.heroCopyFontSize}
+          $weight={font.heroCopyWeight.value}>
+          {font.heroCopy.heroCopy}
+        </HeroCopy>
         <Text>View this font</Text>
       </InternalLink>
     </FontHeroWrapper>
