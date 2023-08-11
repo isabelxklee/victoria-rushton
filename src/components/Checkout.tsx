1f8258ed6b459a641e44dd78ef65b7f1ee262b61
isabelxklee <hi@kleetime.com>
2023-08-10 21:42:02 -0400

    updating checkout component

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from 'react';
+
+import { Button } from '../styles';
 
 const Checkout = () => {
-  return <></>;
+  const [buttonLabel, setButtonLabel] = useState('Checkout');
+
+  useEffect(() => {
+    setButtonLabel(
+      selectedLicense && selectedLicense.title === 'Trial'
+        ? 'Download'
+        : 'Checkout'
+    );
+  }, [selectedLicense]);
+
+  const handleSubmit = async (event: any) => {
+    setButtonLabel('Loading...');
+
+    event.preventDefault();
+
+    const stripe = await loadStripe(process.env.REACT_APP_STRIPE_SECRET);
+    const response = await fetch(
+      `${process.env.REACT_APP_SERVER_URL}/create-checkout-session`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          font: currentFont.title,
+          selectedFonts: selectedFonts,
+          license: selectedLicense
+        })
+      }
+    )
+      .then(r => r.json())
+      .then(session => stripe.redirectToCheckout({ sessionId: session.id }));
+
+    if (!response) {
+      console.log(response.error);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Button
+          type="submit"
+          // $disabled={!selectedLicense || selectedFonts.length < 1}
+        >
+          {buttonLabel}
+        </Button>
+      </form>
+      {/* {selectedLicense && selectedLicense.title === 'Trial' ? (
+        <FormikForm
+          selectedLicense={selectedLicense}
+          selectedFonts={selectedFonts}
+        />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Button
+            type="submit"
+            $disabled={!selectedLicense || selectedFonts.length < 1}>
+            {buttonLabel}
+          </Button>
+        </form>
+      )} */}
+    </>
+  );
 };
 
 export default Checkout;
