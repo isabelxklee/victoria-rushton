869fd6b9a0e12f5b238fa151deae515a59643f25
isabelxklee <hi@kleetime.com>
2023-10-23 13:26:55 -0400

    created success page

---
+++ src/pages/success.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import React from 'react';
+
+// import { ExternalLink } from '../components/Links';
+import PageTemplate from '../components/PageTemplate';
+// import { FlexContainer } from '../components/styles';
+// import { BREAKPOINTS, Button, COLORS, FONT_WEIGHTS, H2, Text } from '../styles';
+
+const Success = () => {
+  return (
+    <PageTemplate>
+      <p>Congratulations on your purchase!</p>
+    </PageTemplate>
+  );
+};
+
+export default Success;
