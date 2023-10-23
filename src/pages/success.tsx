2a4ff131e719fcb353f15d9945544b1a67414856
isabelxklee <hi@kleetime.com>
2023-10-23 13:41:11 -0400

    wrote logic for receiving UUID from backend

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // import { ExternalLink } from '../components/Links';
 import PageTemplate from '../components/PageTemplate';
@@ -8,6 +8,25 @@ import PageTemplate from '../components/PageTemplate';
 // import { BREAKPOINTS, Button, COLORS, FONT_WEIGHTS, H2, Text } from '../styles';
 
 const Success = () => {
+  const [uuid, setUuid] = useState<string>('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${process.env.GATSBY_SERVER_URL}/uuid`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+      const data = await response.json();
+
+      setUuid(data.message);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(uuid);
+
   return (
     <PageTemplate>
       <p>Congratulations on your purchase!</p>
