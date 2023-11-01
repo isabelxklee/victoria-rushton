bc12acaef04b76d45e1f725928d38896086b6c8d
isabelxklee <hi@kleetime.com>
2023-11-01 12:56:43 -0400

    installed formik

--- src/templates/downloadTrialTemplate.tsx
+++ src/templates/downloadTrialTemplate.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import PageTemplate from '../components/PageTemplate';
+import { H2 } from '../styles';
+
+const Wrapper = styled.div`
+  padding: 60px 0;
+`;
 
 const DownloadTrialTemplate = () => {
-  return <></>;
+  return (
+    <PageTemplate>
+      <Wrapper>
+        <H2>Download trial fonts</H2>
+      </Wrapper>
+    </PageTemplate>
+  );
 };
 
 export default DownloadTrialTemplate;
