72686e86ae45114911591056e0ff4059f43c83bb
isabelxklee <hi@kleetime.com>
2023-11-01 13:06:11 -0400

    updated form style

--- src/components/DownloadForm.tsx
+++ src/components/DownloadForm.tsx
@@ -1,11 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Form, Formik } from 'formik';
+import styled from 'styled-components';
 import * as Yup from 'yup';
+
+import { Button, ColumnFlex } from '../styles';
 
 interface DownloadFormProps {
   font: string;
 }
+
+const FormWrapper = styled(ColumnFlex)`
+  gap: 20px;
+  width: 250px;
+`;
 
 const DownloadForm = ({ font }: DownloadFormProps) => {
   const formSchema = Yup.object().shape({
@@ -16,7 +24,7 @@ const DownloadForm = ({ font }: DownloadFormProps) => {
   });
 
   const handleSubmit = async (values: any) => {
-    const response = await fetch(
+    const response: any = await fetch(
       `${process.env.GATSBY_SERVER_URL}/download-trial-fonts`,
       {
         method: 'POST',
@@ -29,16 +37,43 @@ const DownloadForm = ({ font }: DownloadFormProps) => {
       }
     );
 
-    // if (!response) {
-    //   console.log(response.error);
-    // }
+    if (!response) {
+      console.log(response.error);
+    }
 
     // setTimeout(() => {
     //   navigate('/success');
     // }, 900);
   };
 
-  return <></>;
+  return (
+    <>
+      <Formik
+        initialValues={{ name: '', email: '' }}
+        validationSchema={formSchema}
+        onSubmit={values => {
+          handleSubmit(values);
+        }}>
+        {({ errors, touched, isValid, dirty }) => (
+          <Form>
+            <FormWrapper>
+              <label>Name</label>
+              <input autoComplete="off" name="name" type="name" />
+              {/* {errors.name && touched.name && (
+                  <Error name="name">{errors.name}</Error>
+                )} */}
+              <label>Email address</label>
+              <input autoComplete="off" name="email" type="email" />
+              {/* {errors.email && touched.email && (
+                  <Error name="email">{errors.email}</Error>
+                )} */}
+              <Button type="submit">Email trial fonts</Button>
+            </FormWrapper>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
 };
 
 export default DownloadForm;
