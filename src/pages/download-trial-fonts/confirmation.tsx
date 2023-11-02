f88711543fc1adbcc50bf25345667aef0f54695b
isabelxklee <hi@kleetime.com>
2023-11-01 23:45:45 -0400

    updated confirmation page

--- src/pages/download-trial-fonts/confirmation.tsx
+++ src/pages/download-trial-fonts/confirmation.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import { InternalLink } from '../../components/Links';
 import PageTemplate from '../../components/PageTemplate';
-import { Wrapper } from '../../styles';
+import { H2, SecondaryButton, Text } from '../../styles';
+
+const Wrapper = styled.div`
+  padding: 60px 0;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 40px;
+`;
 
 const Confirmation = () => {
   return (
     <PageTemplate>
       <Wrapper>
-        {/* <H2>Download trial fonts</H2>
-        <Text>
-          You will receive an email containing all trial font files once you’ve
-          submitted the form.
-        </Text> */}
+        <H2>Thank you!</H2>
+        <Text>An email has been sent containing your trial font files.</Text>
+        <ButtonWrapper>
+          <SecondaryButton>
+            <InternalLink hideActive={true} url="/">
+              Back to fonts
+            </InternalLink>
+          </SecondaryButton>
+        </ButtonWrapper>
       </Wrapper>
     </PageTemplate>
   );
