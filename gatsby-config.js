c55e96e7660542d8e17c2b2bd6cd0142f97db5fa
isabelxklee <hi@kleetime.com>
2023-11-06 10:59:38 -0500

    deleted everything

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,5 @@ module.exports = {
     title: 'victoria-rushton',
     siteUrl: 'https://victoria-rushton.netlify.app'
   },
-  plugins: [
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        accessToken: 'hBGJYw7pEwG4qZn7cYJon0dHshXjrq3tmHxZd-fFNUc',
-        spaceId: '6l1e28rigfdw'
-      }
-    },
-    'gatsby-plugin-image',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-styled-components'
-  ]
+  plugins: ['gatsby-plugin-styled-components']
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,11 @@
-/* eslint-disable @typescript-eslint/no-use-before-define */
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
 
-import FontHero from '../components/FontHero';
-import PageTemplate from '../components/PageTemplate';
-
-export interface FontType {
-  heroCopy: {
-    heroCopy: string;
-  };
-  heroCopyFontSize: number;
-  heroCopyLineHeight: number;
-  heroCopySlant: {
-    title: string;
-  };
-  heroCopyWeight: {
-    title: string;
-    value: number;
-  };
-  name: string;
-  slants: {
-    title: string;
-  }[];
-  slug: string;
-  weights: {
-    title: string;
-    value: number;
-  }[];
-}
-
-const IndexPage = () => {
-  const data = useStaticQuery(pageQuery);
-  const fonts = data.allContentfulFont.nodes;
-
+const Index = () => {
   return (
-    <PageTemplate>
-      {fonts.map((font: FontType, index: number) => (
-        <FontHero key={index} font={font} />
-      ))}
-    </PageTemplate>
+    <>
+      <p>hello world</p>
+    </>
   );
 };
 
-export default IndexPage;
-
-const pageQuery = graphql`
-  query {
-    allContentfulFont {
-      nodes {
-        name
-        slug
-        heroCopy {
-          heroCopy
-        }
-        heroCopyFontSize
-        heroCopyLineHeight
-        heroCopySlant {
-          title
-        }
-        heroCopyWeight {
-          value
-          title
-        }
-        weights {
-          title
-          value
-        }
-        slants {
-          title
-        }
-      }
-    }
-  }
-`;
+export default Index;
