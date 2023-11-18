961d8d2ee90f82bf02262e67841f581a908b5ac1
isabelxklee <hi@kleetime.com>
2023-11-18 13:06:53 -0500

    updated font files

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,16 +1,11 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import PageTemplate from '../../components/PageTemplate';
-import { Button, ColumnFlex, H2, Text, TextLink } from '../../styles';
+import { H2, Text, TextLink } from '../../styles';
 
 const Wrapper = styled.div`
   padding: 120px 160px;
-`;
-
-const TextWrapper = styled(ColumnFlex)`
-  gap: 20px;
-  margin-bottom: 40px;
 `;
 
 const Success = () => {
@@ -18,16 +13,14 @@ const Success = () => {
     <PageTemplate>
       <Wrapper>
         <H2>Thank you for your purchase!</H2>
-        <TextWrapper>
-          <Text>
-            Your font files have been emailed to you. If you have any issues,
-            please email Victoria at{' '}
-            <TextLink href="mailto:hello@victoriarushton.com">
-              hello@victoriarushton.com
-            </TextLink>{' '}
-            if you have any questions!
-          </Text>
-        </TextWrapper>
+        <Text>
+          Your font files have been emailed to you. If you have any issues,
+          please email Victoria at{' '}
+          <TextLink href="mailto:hello@victoriarushton.com">
+            hello@victoriarushton.com
+          </TextLink>{' '}
+          if you have any questions!
+        </Text>
       </Wrapper>
     </PageTemplate>
   );
