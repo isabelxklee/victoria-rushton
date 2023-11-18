4ac993b3a38d1742a725aa897f406d6bbf8b9b55
isabelxklee <hi@kleetime.com>
2023-11-18 13:06:21 -0500

    updated success page

---
+++ src/pages/success/index.tsx
@@ -0,0 +1,36 @@
+import React, { useMemo } from 'react';
+import styled from 'styled-components';
+
+import PageTemplate from '../../components/PageTemplate';
+import { Button, ColumnFlex, H2, Text, TextLink } from '../../styles';
+
+const Wrapper = styled.div`
+  padding: 120px 160px;
+`;
+
+const TextWrapper = styled(ColumnFlex)`
+  gap: 20px;
+  margin-bottom: 40px;
+`;
+
+const Success = () => {
+  return (
+    <PageTemplate>
+      <Wrapper>
+        <H2>Thank you for your purchase!</H2>
+        <TextWrapper>
+          <Text>
+            Your font files have been emailed to you. If you have any issues,
+            please email Victoria at{' '}
+            <TextLink href="mailto:hello@victoriarushton.com">
+              hello@victoriarushton.com
+            </TextLink>{' '}
+            if you have any questions!
+          </Text>
+        </TextWrapper>
+      </Wrapper>
+    </PageTemplate>
+  );
+};
+
+export default Success;
