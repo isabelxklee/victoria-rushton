3e9095b05bb5d49b19b200ce292d326dfda20c37
isabelxklee <hi@kleetime.com>
2023-11-18 13:08:26 -0500

    added back to fonts button

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { InternalLink } from '../../components/Links';
 import PageTemplate from '../../components/PageTemplate';
-import { H2, Text, TextLink } from '../../styles';
+import { Button, ColumnFlex, H2, Text, TextLink } from '../../styles';
 
 const Wrapper = styled.div`
   padding: 120px 160px;
+`;
+
+const TextWrapper = styled(ColumnFlex)`
+  gap: 20px;
+  margin-bottom: 40px;
 `;
 
 const Success = () => {
@@ -13,14 +19,21 @@ const Success = () => {
     <PageTemplate>
       <Wrapper>
         <H2>Thank you for your purchase!</H2>
-        <Text>
-          Your font files have been emailed to you. If you have any issues,
-          please email Victoria at{' '}
-          <TextLink href="mailto:hello@victoriarushton.com">
-            hello@victoriarushton.com
-          </TextLink>{' '}
-          if you have any questions!
-        </Text>
+        <TextWrapper>
+          <Text>
+            Your font files have been emailed to you. If you have any issues,
+            please email Victoria at{' '}
+            <TextLink href="mailto:hello@victoriarushton.com">
+              hello@victoriarushton.com
+            </TextLink>{' '}
+            if you have any questions!
+          </Text>
+        </TextWrapper>
+        <Button>
+          <InternalLink light={true} url="/">
+            Back to fonts
+          </InternalLink>
+        </Button>
       </Wrapper>
     </PageTemplate>
   );
